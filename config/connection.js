03dcbbfff6c544bbb6cd64b8451516bd9f07240c
Max <maxniederluecke@gmail.com>
2019-01-08 00:24:56 -0600

    JAWS

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,6 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "tpq8om8o1yl2g17b",
-  password: "po33xodi9yfk0qki",
-  database: "l5p5af8yixkx9jnk"
-});
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect(function(err) {
   if (err) {
