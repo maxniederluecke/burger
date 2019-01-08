1a7bee8e9cf4cf10ddabe239114abe92d9f152a7
Max <maxniederluecke@gmail.com>
2019-01-08 00:18:21 -0600

    heroku fix

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "mysql://bbd0350e34c75c:6804b3ef@us-cdbr-iron-east-01.cleardb.net/heroku_6730758dc734163?reconnect=true",
+  host: "z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "vegetable",
-  database: "burgers_db"
+  user: "tpq8om8o1yl2g17b",
+  password: "po33xodi9yfk0qki",
+  database: "l5p5af8yixkx9jnk"
 });
 
 connection.connect(function(err) {
